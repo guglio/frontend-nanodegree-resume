278a6591bb87f0eb3332a8295e116e079fe57ee5
Guglielmo Turco <guglielmo.turco@gmail.com>
2016-10-18 14:50:30 +0200

    FUN

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,5 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append(["Guglielmo Turco"]);
 var awesomeThoughts = "I am Guglielmo Turco and I'm AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
