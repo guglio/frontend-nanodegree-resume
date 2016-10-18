e24e17351fe061a015b1909a3ceec5191e4edd2f
Guglielmo Turco <guglielmo.turco@gmail.com>
2016-10-18 14:57:51 +0200

    Add Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append(["Guglielmo Turco"]);
-var awesomeThoughts = "I am Guglielmo Turco and I'm AWESOME!";
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+// $("#main").append(["Guglielmo Turco"]);
+// var awesomeThoughts = "I am Guglielmo Turco and I'm AWESOME!";
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// $("#main").append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%","Guglielmo Turco");
+var formattedRole = HTMLheaderRole.replace("%data%","Web Developer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
