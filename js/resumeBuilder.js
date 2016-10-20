969ef724c2293fc5dcb474c58cd9db226663198b
Guglielmo Turco <guglielmo.turco@gmail.com>
2016-10-20 23:02:41 +0200

    Add contact information, picture url, welcome message and skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,9 +5,34 @@ This is empty on purpose! Your code to build the resume will go here.
 // var awesomeThoughts = "I am Guglielmo Turco and I'm AWESOME!";
 // var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 // $("#main").append(funThoughts);
-var name = "Guglielmo Turco";
-var role =  "Web Developer";
-var formattedName = HTMLheaderName.replace("%data%",name);
-var formattedRole = HTMLheaderRole.replace("%data%",role);
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+// var name = "Guglielmo Turco";
+// var role =  "Web Developer";
+// var formattedName = HTMLheaderName.replace("%data%",name);
+// var formattedRole = HTMLheaderRole.replace("%data%",role);
+// $("#header").append(formattedName);
+// $("#header").append(formattedRole);
+
+var bio = {
+          "name" : "Guglielmo Turco",
+          "role" : "Web Developer",
+          "contacts" : {
+            "mobile" : "+39 3463809631",
+            "email" : "guglielmo.turco@gmail.com",
+            "github" : "guglio",
+            "twitter" : "@gugliolo",
+            "location" : "Cuneo"
+          },
+          "pictureURL" : "images/me.jpg",
+          "welcomeMsg" : "Welcome!",
+          "skills" : ["HTML5", "CSS3", "JavaScript", "Web Developer"]
+ };
+$("#header").append(HTMLheaderName.replace("%data%",bio.name));
+$("#header").append(HTMLheaderRole.replace("%data%",bio.role));
+$("#header").append(HTMLmobile.replace("%data%",bio.contacts.mobile));
+$("#header").append(HTMLemail.replace("%data%",bio.contacts.email));
+$("#header").append(HTMLtwitter.replace("%data%",bio.contacts.twitter));
+$("#header").append(HTMLgithub.replace("%data%",bio.contacts.github));
+$("#header").append(HTMLlocation.replace("%data%",bio.contacts.location));
+$("#header").append(HTMLbioPic.replace("%data%",bio.pictureURL));
+$("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomeMsg));
+$("#header").append(HTMLskillsStart + HTMLskills.replace("%data%",bio.skills));
