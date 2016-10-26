ff4787a2cc277c168a0981d98686e4358267185c
Guglielmo Turco <guglielmo.turco@gmail.com>
2016-10-26 10:49:04 +0200

    Add json scheme

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,51 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-
+//  BIO
+//  name : string
+//        role : string
+//        contacts : an object with
+//              mobile: string
+//              email: string
+//              github: string
+//              twitter: string (optional)
+//              location: string
+//        welcomeMessage: string
+//        skills: array of strings
+//        biopic: url
+// display: function
+//
+// EDUCATION
+// schools: array of objects with
+//            name: string
+//            location: string
+//            degree: string
+//            majors: array of strings
+//            dates: string (works with a hyphen between them)
+//            url: string
+// onlineCourses: array of objects with
+//            title: string
+//            school: string
+//            dates: string (works with a hyphen between them)
+//            url: string
+// display: function
+//
+// WORK
+// jobs: array of objects with
+//            employer: string
+//            title: string
+//            location: string
+//            dates: string (Can be 'in progress')
+//            description: string
+// display: function
+//
+// PROJECTS
+// projects: array of objects with
+//             title: string
+//             dates: string (works with a hyphen between them)
+//             description: string
+//             images: array with string urls
+// display: function
 
 var bio = {
           "name" : "Guglielmo Turco",
